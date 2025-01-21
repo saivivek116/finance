87ef774f74aeebedb71114d0d3748f9c70498f9d
reysree <sreeram.bangaroo@gmail.com>
2025-01-20 23:27:36 -0500

    Added card component to dashboard

--- src/app/(main)/dashboard/page.jsx
+++ src/app/(main)/dashboard/page.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import CreateAccountDrawer from '@/components/create-account-drawer';
+import { Card, CardContent } from '@/components/ui/card';
+import { Plus } from 'lucide-react';
 
 const Dashboard = () => {
   return (
     <div className="px-5">
-      <CreateAccountDrawer></CreateAccountDrawer>
+      <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
+      <CreateAccountDrawer>
+        <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
+          <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+            <Plus className='h-10 w-10 mb-2'/>
+            <p className='text-sm font-medium'>Add new Account</p>
+          </CardContent>
+        </Card>
+      </CreateAccountDrawer>
+      </div>
     </div>
   );
 };
--- src/components/create-account-drawer.jsx
+++ src/components/create-account-drawer.jsx
@@ -3,10 +3,11 @@
 import React from 'react';
 import { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle } from './ui/drawer';
 
-const CreateAccountDrawer = () => {
+const CreateAccountDrawer = ({children}) => {
+  const [open, setOpen] = React.useState(false);
   return (
-    <Drawer>
-      <DrawerTrigger>Open</DrawerTrigger>
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
       <DrawerContent>
         <DrawerHeader>
           <DrawerTitle>Test</DrawerTitle>
